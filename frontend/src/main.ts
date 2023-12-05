c84d66ba0c699321c645b918a6138f79cd8720bd
filippo16 <filippo.lutterotti2@gmail.com>
2023-12-05 21:54:55 +0100

    Modify main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,15 @@ import { createRouter, createWebHistory, Router } from 'vue-router'
 import App from './App.vue'
 
 import Home from './pages/Home.vue'
+import Login from './pages/Login.vue';
+import Esplora from './pages/Esplora.vue';
 
 const router: Router = createRouter ({
     history: createWebHistory(),
     routes: [
-        {path: "/", component: Home}
+        {path: "/", component: Home},
+        {path: "/login", component: Login},
+        {path: "/explore", component: Esplora}
     ]
 });
 
