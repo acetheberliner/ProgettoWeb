b898777f9b9ac44848297d46eccca7dcd9982738
filippo16 <filippo.lutterotti2@gmail.com>
2023-12-05 22:11:01 +0100

    Add notes-category

--- backend/src/controllers/notes-controller.ts
+++ backend/src/controllers/notes-controller.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express'
 import { connection } from '../utils/db'
 
+// tutte le note
 export async function allNotes(req: Request, res: Response) {
     // await getConnection
     connection.execute(
         "SELECT titolo, categoria, autore, testo FROM note",
         [],
+        function (err, results, fields) {
+            res.json(results);
+        }
+    )
+}
+
+// note per categoria
+export async function notesPerCategory(req: Request, res: Response) {
+    // await getConnection
+    connection.execute(
+        "SELECT titolo, categoria, autore, testo FROM note WHERE categoria = ?",
+        [req.params.n],
         function (err, results, fields) {
             res.json(results);
         }
--- backend/src/routes/notes-router.ts
+++ backend/src/routes/notes-router.ts
@@ -4,5 +4,6 @@ import * as notaController from '../controllers/notes-controller'
 const router: Router = Router()
 
 router.get("/api/note", notaController.allNotes) 
+router.get("/api/note/:n", notaController.notesPerCategory)
 
 export default router
