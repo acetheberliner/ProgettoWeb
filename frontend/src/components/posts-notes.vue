0a242940fd596e31fc8ae2391e038e607b7e2dd4
filippo16 <filippo.lutterotti2@gmail.com>
2023-12-06 15:55:16 +0100

    Create component

---
+++ frontend/src/components/posts-notes.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { PropType, defineComponent } from 'vue';
+    import { Nota } from '../types';
+
+    export default defineComponent ({
+        props: {
+            nota: {
+                type: Object as PropType<Nota>,
+                required: true
+            }
+        }
+    })
+</script>
+
+<template>
+    <article>
+        <h3>{{nota.titolo}}</h3>
+        <p>{{nota.categoria}}</p>
+        <p>{{nota.autore}}</p>
+        <p>{{nota.testo}}</p>
+    </article>
+</template>
