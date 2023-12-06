fd576b8f8ad94232be26d2eabf283c14e7c638df
filippo16 <filippo.lutterotti2@gmail.com>
2023-12-06 14:52:11 +0100

    move home in esplora

--- frontend/src/pages/Esplora.vue
+++ frontend/src/pages/Esplora.vue
@@ -1,3 +1,32 @@
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import axios from 'axios'
+    import { Nota } from '../types'
+
+    export default defineComponent({
+        data() {
+            return {
+                datiNote: [] as Nota[]
+            }
+        },
+        methods: {
+            getNote() {
+                axios.get("/api/note")
+                    .then(response => this.datiNote = response.data)
+            }
+        },
+        mounted() {
+            this.getNote()
+        }
+    })
+</script>
+
 <template>
-    <h1>Esplora</h1>
+    <h2>Note di Studenti</h2>
+    <article v-for="nota in datiNote">
+        <h3>{{nota.titolo}}</h3>
+        <p>{{nota.categoria}}</p>
+        <p>{{nota.autore}}</p>
+        <p>{{nota.testo}}</p>
+    </article>
 </template>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,32 +1,5 @@
-<script lang="ts">
-    import { defineComponent } from 'vue'
-    import axios from 'axios'
-    import { Nota } from '../types'
 
-    export default defineComponent({
-        data() {
-            return {
-                datiNote: [] as Nota[]
-            }
-        },
-        methods: {
-            getNote() {
-                axios.get("/api/note")
-                    .then(response => this.datiNote = response.data)
-            }
-        },
-        mounted() {
-            this.getNote()
-        }
-    })
-</script>
 
 <template>
-    <h2>Note di Studenti</h2>
-    <article v-for="nota in datiNote">
-        <h3>{{nota.titolo}}</h3>
-        <p>{{nota.categoria}}</p>
-        <p>{{nota.autore}}</p>
-        <p>{{nota.testo}}</p>
-    </article>
+  
 </template>
