63a92880d3a8a14bf53fe61a449d9dab7eb6c099
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-01-03 16:02:13 +0100

    modifica del collegmaneto al db

--- backend/src/controllers/notes-controller.ts
+++ backend/src/controllers/notes-controller.ts
@@ -1,24 +1,23 @@
-import { Request, Response } from 'express'
-import { getConnection } from '../utils/db'
-
+import { Request, Response } from "express";
+import { getConnection } from "../utils/db";
 
 // tutte le note
 export async function allNotes(req: Request, res: Response) {
-    const connection = await getConnection()
-    const [results] = await connection.execute(
-        "SELECT titolo, categoria, data, autore, testo FROM note",
-        []
-    )
+  const connection = await getConnection();
+  const [results] = await connection.execute(
+    "SELECT titolo, categoria, data, autore, testo FROM note",
+    []
+  );
 
-    res.json(results)
+  res.json(results);
 }
 
 // note per categoria
 export async function notesCategory(req: Request, res: Response) {
-    const connection = await getConnection()
-    const [results] = await connection.execute(
-        "SELECT titolo, categoria, data, autore, testo FROM note WHERE categoria = ?",
-        [req.params.n]
-    )
-    res.json(results)
+  const connection = await getConnection();
+  const [results] = await connection.execute(
+    "SELECT titolo, categoria, data, autore, testo FROM note WHERE categoria = ?",
+    [req.params.n]
+  );
+  res.json(results);
 }
