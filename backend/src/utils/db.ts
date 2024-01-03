12d748586af3d9e319f5daddeba61634391ec2fe
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-01-03 16:02:41 +0100

    modifica del collegmaneto al db

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,15 +1,15 @@
-import mysql, { Connection } from 'mysql2/promise'
+import mysql, { Connection } from "mysql2/promise";
 
-let connection: Connection | null = null
+let connection: Connection | null = null;
 
 export async function getConnection() {
-    if(!connection) {
-        connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'progetto_prova1'
-        })
-    }
-    return connection
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "porgetto_prova1",
+    });
+  }
+  return connection;
 }
