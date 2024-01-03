27964c7fe68becf3ae0f98c1efaedae2539ae959
filippo16 <filippo.lutterotti2@gmail.com>
2024-01-03 12:37:27 +0100

    aggiunto creazione file sql

---
+++ backend/sql/creazione_db.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS porgetto_prova1;
+
+CREATE DATABASE porgetto_prova1;
+
+USE porgetto_prova1;
+
+CREATE TABLE `note` (
+  `idnote` int(11) NOT NULL,
+  `titolo` varchar(20) NOT NULL,
+  `categoria` varchar(45) NOT NULL,
+  `data` varchar(20) NOT NULL,
+  `autore` varchar(20) NOT NULL,
+  `testo` varchar(2500) NOT NULL,
+  PRIMARY KEY (`idnote`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  
+  CREATE TABLE `utenti` (
+  `username` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
