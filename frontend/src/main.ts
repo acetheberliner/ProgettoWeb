6e424c8a12800120f566a88f066f8203910357ba
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-01-05 16:52:14 +0100

    Modifiche seguendo laboratori in classe

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,28 +1,43 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory, Router } from 'vue-router'
-import App from './App.vue'
+// import axios from "axios";
+import { createApp } from "vue";
+import { createRouter, createWebHistory, Router } from "vue-router";
+import App from "./App.vue";
 
-import Home from './pages/Home.vue'
-import Login from './pages/Login.vue'
-import Esplora from './pages/Esplora.vue'
-import Register from './pages/Register.vue'
-import MyAccount from './pages/MyAccount.vue'
-import DoANote from './pages/DoANote.vue'
-import NotFound from './pages/NotFound.vue'
+import Home from "./pages/Home.vue";
+import Login from "./pages/Login.vue";
+import Esplora from "./pages/Esplora.vue";
+import Register from "./pages/Register.vue";
+import MyAccount from "./pages/MyAccount.vue";
+import DoANote from "./pages/DoANote.vue";
+import NotFound from "./pages/NotFound.vue";
 
-const router: Router = createRouter ({
-    history: createWebHistory(),
-    routes: [
-        {path: "/", component: Home},
-        {path: "/login", component: Login},
-        {path: "/explore", component: Esplora},
-        {path: "/register", component: Register},
-        {path: "/myaccount", component: MyAccount},
-        {path: "/doanote", component: DoANote},
-        {path: "/:pathMatch(.*)*", component: NotFound}
-    ]
+// import { User } from "./types";
+
+const router: Router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/login", component: Login },
+    { path: "/explore", component: Esplora },
+    { path: "/register", component: Register },
+    { path: "/myaccount", component: MyAccount },
+    { path: "/doanote", component: DoANote },
+    { path: "/:pathMatch(.*)*", component: NotFound },
+  ],
 });
 
-createApp(App)
-    .use(router)
-    .mount("#app")
+// Funzione che viene eseguita prima di ogni navigazione del router
+// router.beforeEach(async (to) => {
+//   const res = await axios.get("/api/auth/profile");
+//   const user = res.data as User | null;
+//   // Se la pagina richiede il login, ma l'utente non l'ha effettuato, lo rimanda alla pagina di login
+//   if (to.meta.requireLogin && !user) {
+//     return { path: "/login" };
+//   }
+//   // Se la pagina richiede il logout, ma l'utente ha effettuato l'accesso, lo rimanda alla home
+//   if (to.meta.requireLogout && user) {
+//     return { path: "/" };
+//   }
+// });
+
+createApp(App).use(router).mount("#app");
