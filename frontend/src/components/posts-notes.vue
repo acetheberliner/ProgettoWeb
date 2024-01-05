73a9abb4dd6799b72ee3b2e4c3a6490c47a20672
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-01-05 13:23:09 +0100

    Aggiunta possibilità di aprire le note in overlay

--- frontend/src/components/posts-notes.vue
+++ frontend/src/components/posts-notes.vue
@@ -23,12 +23,134 @@ export default defineComponent({
       <p class="anteprima">{{ nota.anteprima }}</p>
     </section>
     <button>
-      <span> Visualizza </span>
+      <span
+        ><a
+          id="open"
+          href="javascript:void(0)"
+          onclick="document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'"
+          >Visualizza</a
+        ></span
+      >
     </button>
   </div>
+  <div id="light" class="white_content">
+    <div class="title">
+      <h2>
+        {{ nota.titolo }}
+        <div class="svgicon">
+          <img src="/paper-document-svgrepo-com.svg" alt="" />
+        </div>
+      </h2>
+    </div>
+    <div class="secondary_info">
+      {{ nota.categoria }}<br />
+      {{ nota.autore }}<br />
+      {{ nota.data }}
+    </div>
+    <hr />
+    <p id="inner_note">{{ nota.testo }}</p>
+    <a
+      id="close"
+      href="javascript:void(0)"
+      onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'"
+      >Chiudi</a
+    >
+    <!-- <a
+      id="download"
+      href="../../public/texts/{{& nota.titolo }}.txt"
+      download=""
+      >Scarica</a
+    > -->
+  </div>
+  <div id="fade" class="black_overlay"></div>
 </template>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Quicksand&display=swap");
+/*- ANTEPRIMA -------------------------------------------------------------------------------------------------------------------*/
+h2 {
+  font-weight: bold;
+  margin-left: 10px;
+  margin-top: 10px;
+}
+
+img {
+  width: 50px;
+  margin-right: 15px;
+}
+
+.svgicon {
+  text-align: end;
+  margin-right: -12px;
+}
+
+a#open {
+  color: white;
+  text-decoration: none;
+}
+
+p#inner_note {
+  color: #183252;
+}
+
+a#close {
+  color: #183252;
+  text-decoration: none;
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  padding: 0.8em 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: none;
+  text-transform: uppercase;
+  background-color: #356cb1;
+  border-radius: 10px;
+  color: #fff;
+  font-weight: 300;
+  font-size: 12px;
+  font-family: inherit;
+  z-index: 0;
+  overflow: hidden;
+  transition: all 0.3s cubic-bezier(0.02, 0.01, 0.47, 1);
+}
+
+.secondary_info {
+  text-align: end;
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.black_overlay {
+  display: none;
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.815);
+  z-index: 1001;
+  -moz-opacity: 0.8;
+  opacity: 0.8;
+  filter: alpha(opacity=80);
+}
+.white_content {
+  display: none;
+  position: absolute;
+  top: 25%;
+  left: 25%;
+  width: 50%;
+  height: 50%;
+  padding: 16px;
+  border: 1px solid gray;
+  border-radius: 10px;
+  background-color: white;
+  color: #183252;
+  z-index: 1002;
+  overflow: auto;
+}
 /*- BUTTON -------------------------------------------------------------------------------------------------------------------*/
 button {
   position: absolute;
@@ -52,7 +174,8 @@ button {
   transition: all 0.3s cubic-bezier(0.02, 0.01, 0.47, 1);
 }
 
-button:hover {
+button:hover,
+a#close:hover {
   animation: sh0 0.5s ease-in-out both;
 }
 
@@ -78,13 +201,15 @@ button:hover {
   }
 }
 
-button:hover span {
+button:hover,
+a#close:hover span {
   animation: storm 0.7s ease-in-out both;
   animation-delay: 0.06s;
 }
 
 button::before,
-button::after {
+a#close:before button::after,
+a#close:after {
   content: "";
   position: absolute;
   right: 0;
@@ -101,17 +226,20 @@ button::after {
 }
 
 button:hover::before,
-button:hover::after {
+a#close:hover::before button:hover::after,
+a#close:hover::after {
   opacity: 0.15;
   transition: transform 0.2s cubic-bezier(0.02, 0.01, 0.47, 1),
     opacity 0.2s cubic-bezier(0.02, 0.01, 0.47, 1);
 }
 
-button:hover::before {
+button:hover::before,
+a#close:hover::before {
   transform: translate3d(50%, 0, 0) scale(0.9);
 }
 
-button:hover::after {
+button:hover::after,
+a#close:hover::after {
   transform: translate(50%, 0) scale(1.1);
 }
 
@@ -134,8 +262,9 @@ button:hover::after {
 }
 
 h3 {
-  font-family: clipFont;
-  font-size: 36px;
+  /* font-family: clipFont; */
+  font-family: "Quicksand", sans-serif;
+  font-size: 30px;
   font-weight: bold;
   text-align: start;
   margin-left: 10px;
