528fd6487708017b540e32c2facb5982b64f5ee9
filippo16 <filippo.lutterotti2@gmail.com>
2024-01-27 11:00:59 +0100

    Aggiunto router post per editPost

--- backend/src/routes/post-notes-router.ts
+++ backend/src/routes/post-notes-router.ts
@@ -5,5 +5,6 @@ const router: Router = Router();
 
 router.post("/api/createPost", postNotesController.createPost);
 router.post("/api/deletePost", postNotesController.deletePost);
+router.post("/api/editPost", postNotesController.editPost)
 
 export default router;
