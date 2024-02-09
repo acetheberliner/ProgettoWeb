d07015de0c0c1cf99b53ab38b72b6b516b000f62
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-02-09 13:11:07 +0100

    Homepage

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -8,42 +8,43 @@ export default defineComponent({});
 
 <template>
   <div class="page">
-    <div class="header1">
-      <div class="title">
-        <h1>NoteSync</h1>
-        <h2>"Condividi ciò che sai"</h2>
-      </div>  
-      <div class="caption">
-        <p>Benvenuto in un nuovo servizio di gestione appunti! La piattaforma offre un'esperienza senza complicazioni, arricchendo il tuo modo di organizzare e accedere alle tue note in modo efficiente. Ecco quali sono le funzionalità principali offerte:</p>
-      </div>
-    </div>
-    <hr>
-    <div class="content">
-      <div class="bubble">
-        <h3>Homepage</h3>  
-        <p>Rappresenta il punto di accesso centrale alla piattaforma di gestione appunti. Qui, gli utenti possono registrarsi o effettuare il login per accedere alle funzionalità di creazione, modifica e organizzazione delle note stesse. </p>
+    <div class="info">
+      <div class="header1">
+        <div class="title">
+          <h1>NoteSync - <span>"Condividi ciò che sai"</span></h1>
+        </div>  
+        <div class="caption">
+          <p>Benvenuto in un nuovo servizio di gestione appunti! La piattaforma offre un'esperienza senza complicazioni, arricchendo il tuo modo di organizzare e accedere alle tue note in modo efficiente. Ecco quali sono le funzionalità principali offerte:</p>
+        </div>
       </div>
       <hr>
-      <div class="bubble">
-        <h3>Profilo</h3>
-        <ul>
-          <li>Registrazione/Accesso</li>
-          <li>Assegnazione Cookie</li>
-          <li><RouterLink to="/myaccount">Sezione profilo utente</RouterLink></li>
-        </ul>
-      </div>
-      <hr>
-      <div class="bubble">
-        <h3>Sezione Appunti</h3>
-        <p>Esplora una ricca collezione di appunti in modo ordinato e strutturato. Ogni nota è visualizzabile singolarmente e filtrabile in base alla categoria che più ti interessa e se vorrai potrai addirittura contribuire all'espansione della piattaforma creandone di nuove.</p>
-        <p>Le funzionalità offerte sono:</p>
-        <ul>
-          <li>Creazione note</li>
-          <li>Modifica note</li>
-          <li>Cancellazione note</li>
-          <li>filtro</li>
-          <li>Visualizzazione ampia</li>
-        </ul>
+      <div class="content">
+        <div class="bubble">
+          <h3>Homepage</h3>  
+          <p>Rappresenta il punto di accesso centrale alla piattaforma di gestione appunti. Qui, gli utenti possono registrarsi o effettuare il login per accedere alle funzionalità di creazione, modifica e organizzazione delle note stesse. </p>
+        </div>
+        <hr>
+        <div class="bubble">
+          <h3>Profilo</h3>
+          <ul>
+            <li>Registrazione/Accesso</li>
+            <li>Assegnazione Cookie</li>
+            <li><RouterLink to="/myaccount">Sezione profilo utente</RouterLink></li>
+          </ul>
+        </div>
+        <hr>
+        <div class="bubble">
+          <h3>Appunti</h3>
+          <p>Esplora una ricca collezione di appunti in modo ordinato e strutturato. Ogni nota è visualizzabile singolarmente e filtrabile in base alla categoria che più ti interessa e se vorrai potrai addirittura contribuire all'espansione della piattaforma creandone di nuove.</p>
+          <p>Le funzionalità offerte sono:</p>
+          <ul>
+            <li>Creazione note</li>
+            <li>Modifica note</li>
+            <li>Cancellazione note</li>
+            <li>filtro</li>
+            <li>Visualizzazione ampia</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -59,11 +60,57 @@ export default defineComponent({});
 
 .page {
   display: flex;
-  flex-direction: column;
   align-items: center;
+  justify-content: center;
 }
 
-.header1 {
+.info {
+  border: 1px solid white;
+  border-radius: 12px;
+  background-color: white;
+  color: #183252;
+  width: 80%;
+  padding: 1em;
+  margin-top: 20px;
+}
+
+h1 {
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: default;
+}
+
+h1>span {
+  font-size: 20px;
+  font-style: italic;
+  vertical-align: middle;
+  font-weight: bold;
+  background: linear-gradient(to right, #0af3e0 0%, #038cfc 25%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  text-transform:none;
+}
+
+h3 {
+  font-weight: bold;
+}
+
+hr {
+  border: none;
+  height: 2px;
+  background-color: #235971;
+  margin: auto;
+  width: 80%;
+}
+
+/* .page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+} */
+
+/* .header1 {
   width: 80%;
   border: 1px solid rgba(219, 216, 216, 0.562);
   border-radius: 10px;
@@ -94,9 +141,9 @@ h2 {
 }
 
 .content {
-  /* flex-grow: 1; Per far crescere questo div per occupare lo spazio rimanente */
-  overflow: auto; /* Abilita lo scrolling verticale quando il contenuto supera l'altezza massima */
-  padding: 20px; /* Aggiunge spazio interno per evitare che il contenuto sia troppo vicino ai bordi */
+  /* flex-grow: 1; Per far crescere questo div per occupare lo spazio rimanente
+  overflow: auto; /* Abilita lo scrolling verticale quando il contenuto supera l'altezza massima
+  padding: 20px; /* Aggiunge spazio interno per evitare che il contenuto sia troppo vicino ai bordi 
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   width: 80%;
@@ -113,6 +160,6 @@ h2 {
   background:transparent;
   backdrop-filter:blur(20px);
   margin-bottom: 20px;
-}
+} */
 
 </style>
